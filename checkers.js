10bb4a1033237ab49bdec856e332c329dd96558a
ofirhotz <ofirhotz@gmail.com>
2022-05-01 01:18:17 +0300

    first steps

---
+++ checkers.js
@@ -0,0 +1,16 @@
+const BOARD_SIZE = 8;
+const WHITE_PLAYER = 'white';
+const BLACK_PLAYER = 'black';
+
+const WHITEMAN = 'whiteman';
+const BLACKMAN = 'blackman';
+const WHITEKING = 'whiteking';
+const BLACKKING = 'blackking';
+
+// const PIECES = [ROOK, KNIGHT, BISHOP, KING, QUEEN, BISHOP, KNIGHT, ROOK];
+
+const CHECKERS_BOARD_ID = 'checkers-board';
+
+let game;
+let table;
+let selectedPiece;
