c8c1f23e8ce46ba3371cdf51e20f3520c2e5fb2b
ofirhotz <ofirhotz@gmail.com>
2022-05-01 10:50:56 +0300

    creating checkers board

---
+++ boardData.js
@@ -0,0 +1,50 @@
+class BoardData {
+    constructor() {
+      this.initPieces();
+    }
+  
+    initPieces() {
+      // Create list of pieces (32 total)
+      this.pieces = [];
+      /*
+  
+      for (let i = 0; i < BOARD_SIZE; i++) {
+        this.pieces.push(new Piece(0, i, MAN, WHITE_PLAYER));
+        this.pieces.push(new Piece(1, i,  MAN, WHITE_PLAYER));
+        this.pieces.push(new Piece(2, i,  MAN, WHITE_PLAYER));
+        this.pieces.push(new Piece(5, i, MAN, BLACK_PLAYER));
+        this.pieces.push(new Piece(6, i,  MAN, BLACK_PLAYER));
+        this.pieces.push(new Piece(7, i, MAN, BLACK_PLAYER));
+      } 
+      */
+    }
+  /*
+    // Returns piece in row, col, or undefined if not exists.
+    getPiece(row, col) {
+      for (const piece of this.pieces) {
+        if (piece.row === row && piece.col === col) {
+          return piece;
+        }
+      }
+    }
+  
+    removePiece(row, col) {
+      for (let i = 0; i < this.pieces.length; i++) {
+        const piece = this.pieces[i];
+        if (piece.row === row && piece.col === col) {
+          // Remove piece at index i
+          this.pieces.splice(i, 1);
+          return piece;
+        }
+      }
+    }
+  
+    isEmpty(row, col) {
+      return this.getPiece(row, col) === undefined;
+    }
+  
+    isPlayer(row, col, player) {
+      const piece = this.getPiece(row, col);
+      return piece !== undefined && piece.player === player;
+    } */
+  }
