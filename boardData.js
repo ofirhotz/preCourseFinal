7d00d43a13cda9b191ea7f5a7e0bccdc8e27520d
ofirhotz <ofirhotz@gmail.com>
2022-05-02 23:01:49 +0300

    add king and alert about who win the game

--- boardData.js
+++ boardData.js
@@ -9,7 +9,7 @@ class BoardData {
 
 
     for (let i = 0; i < BOARD_SIZE; i = i + 2) {
-      this.pieces.push(new Piece(0, i+1 , MAN, WHITE_PLAYER));
+      this.pieces.push(new Piece(0, i + 1, MAN, WHITE_PLAYER));
       this.pieces.push(new Piece(1, i, MAN, WHITE_PLAYER));
       this.pieces.push(new Piece(2, i + 1, MAN, WHITE_PLAYER));
       this.pieces.push(new Piece(5, i, MAN, BLACK_PLAYER));
@@ -18,33 +18,53 @@ class BoardData {
     }
 
   }
-  
-    // Returns piece in row, col, or undefined if not exists.
-    getPiece(row, col) {
-      for (const piece of this.pieces) {
-        if (piece.row === row && piece.col === col) {
-          return piece;
-        }
+
+  // Returns piece in row, col, or undefined if not exists.
+  getPiece(row, col) {
+    for (const piece of this.pieces) {
+      if (piece.row === row && piece.col === col) {
+        return piece;
       }
     }
-  
-    removePiece(row, col) {
-      for (let i = 0; i < this.pieces.length; i++) {
-        const piece = this.pieces[i];
-        if (piece.row === row && piece.col === col) {
-          // Remove piece at index i
-          this.pieces.splice(i, 1);
-          return piece;
-        }
+  }
+
+  removePiece(row, col) {
+    for (let i = 0; i < this.pieces.length; i++) {
+      const piece = this.pieces[i];
+      if (piece.row === row && piece.col === col) {
+        // Remove piece at index i
+        this.pieces.splice(i, 1);
+        return piece;
       }
     }
-  
-    isEmpty(row, col) {
-      return this.getPiece(row, col) === undefined;
+  }
+
+  isEmpty(row, col) {
+    return this.getPiece(row, col) === undefined;
+  }
+
+  isPlayer(row, col, player) {
+    const piece = this.getPiece(row, col);
+    return piece !== undefined && piece.player === player;
+  }
+
+  cheackVictory(piece) {
+    const pieces = this.pieces;
+    const opponent = piece.getOpponent();
+    for (let i = 0; i < pieces.length; i++) {
+      if (opponent == pieces[i].player) {
+        return false;
+      }
     }
-  
-    isPlayer(row, col, player) {
-      const piece = this.getPiece(row, col);
-      return piece !== undefined && piece.player === player;
-    } 
+    return true;
+  }
+
+  becomeKing(row, col) {
+    const piece = this.getPiece(row, col);
+    if ((piece.player === WHITE_PLAYER && piece.row == 7) || (piece.player === BLACK_PLAYER && piece.row == 0)) {
+      return true;
+    }
+    return false
+
+  }
 }
--- game.js
+++ game.js
@@ -13,23 +13,18 @@ class Game {
       // possibleMove looks like this: [1,2]
       if (possibleMove[0] === row && possibleMove[1] === col) {
         // There is a legal move
-        console.log(possibleMove[3]);
         if (possibleMove[2] !== undefined && possibleMove[3] !== undefined) {
-          console.log(3);
           const name = this.boardData.removePiece(possibleMove[2], possibleMove[3]);
-          const capturedPiece = this.boardData.getPiece (possibleMove[2], possibleMove[3]) ;
         }
         const removedPiece = this.boardData.removePiece(row, col);
         piece.row = row;
         piece.col = col;
-        console.log(4);
 
-        /*if (possibleMove[3] !== undefined && possibleMove[4] !== undefined) {
-          console.log (4);
-          const name = this.boardData.removePiece(possibleMove[3], possibleMove[4]);
-        } */
-        console.log(5);
-        if (removedPiece !== undefined && removedPiece.type === KING) {
+        if (this.boardData.becomeKing(row, col)) {
+          piece.type = KING;
+
+        }
+        if (this.boardData.cheackVictory(piece)) {
           this.winner = piece.player;
         }
 
@@ -40,35 +35,6 @@ class Game {
     return false;
   }
 
-
-  /*
-    // Tries to actually make a move. Returns true if successful.
-    tryMove(piece, row, col) {
-      const possibleMoves = this.getPossibleMoves(piece);
-      console.log(possibleMoves);
-      // possibleMoves looks like this: [[1,2], [3,2]]
-      for (const possibleMove of possibleMoves) {
-        // possibleMove looks like this: [1,2]
-        if (possibleMove[3] !== undefined && possibleMove[4] !== undefined) {
-          // There is a legal move
-           if (cheacKIfCapturedPiece(row, col) === true) {
-            const capturedPiece = this.boardData.removePiece();
-          } 
-          const removedPiece = this.boardData.removePiece(row, col);
-          piece.row = row;
-          piece.col = col;
-  
-          if (removedPiece !== undefined && removedPiece.type === KING) {
-            this.winner = piece.player;
-          }
-  
-          this.currentPlayer = piece.getOpponent();
-          return true;
-        }
-      }
-      return false;
-    } */
-
   getPossibleMoves(piece) {
     if (this.currentPlayer !== piece.player || this.winner !== undefined) {
       return [];
@@ -76,13 +42,7 @@ class Game {
     return piece.getPossibleMoves(this.boardData);
   }
 
-  // function cheak if piece Captured opponent piece
-  /*cheacKIfCapturedPiece(row, col) {
-   if () {
-
-   } */
 }
-
 
 
 
